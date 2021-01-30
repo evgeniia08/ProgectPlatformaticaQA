27c214bf0a32dad1e756a902ed3a6df787cb804b
OxanaE <onermol@gmail.com>
2021-01-30 16:54:43 -0500

    added assert number of records after save

--- src/test/java/test/entity/EntityDefaultTest.java
+++ src/test/java/test/entity/EntityDefaultTest.java
@@ -155,9 +155,10 @@ public class EntityDefaultTest extends BaseTest {
                 changedEmbeddedValues.fieldText, changedEmbeddedValues.fieldInt, changedEmbeddedValues.fieldDecimal,
                 changedEmbeddedValues.fieldDate, changedEmbeddedValues.fieldDateTime, changedEmbeddedValues.fieldUser);
 
-        defaultEditPage
-                .clickSaveButton()
-                .viewRow(0);
+        DefaultPage defaultPage = defaultEditPage.clickSaveButton();
+
+        Assert.assertEquals(defaultPage.getRowCount(), 1);
+        defaultPage.viewRow(0);
 
         assertRecordValues(driver, "//span[@class='pa-view-field']", CHANGED_DEFAULT_VALUES);
 
