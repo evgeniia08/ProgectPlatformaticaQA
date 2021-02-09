2486db470a0285062e70ad967aa1437eb606ad23
Stan Aretinskiy <sr575sr@gmail.com>
2021-02-08 20:45:18 -0800

    fix EntityPlaceholderTest (#590)
     - POM
     - extend test coverage
     - delete duplicate test
     - delete duplicate pages

--- src/test/java/model/entity/table/PlaceholderPage.java
+++ src/test/java/model/entity/table/PlaceholderPage.java
@@ -1,49 +1,25 @@
-
 package model.entity.table;
 
 import model.base.EntityBaseTablePage;
-import model.entity.edit.PlaceholderEdit1Page;
 import model.entity.edit.PlaceholderEditPage;
 import model.entity.view.PlaceholderViewPage;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
-import org.testng.Assert;
-import runner.ProjectUtils;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
-public final class PlaceholderPage extends EntityBaseTablePage<PlaceholderPage, PlaceholderEdit1Page, PlaceholderViewPage> {
-
-    @FindBy(xpath = "//i[contains(text(),'create_new_folder')]")
-    private WebElement newFolderButton;
-
-    @FindBy(xpath = "//tr/td[2]/a/div")
-    WebElement el1;
-
-    @FindBy(xpath = "//tr/td[3]/a/div")
-    WebElement el2;
-
-    @FindBy(xpath = "//tr/td[4]/a/div")
-    WebElement el3;
-
-    @FindBy(xpath = "//tr/td[5]/a/div")
-    WebElement el4;
-
-    @FindBy(xpath = "//tr[@data-index='0']//button/i[text()='menu']")
-    WebElement actions;
-
-    @FindBy(xpath = "//tr[@data-index='0']//div//li/a[text()='delete']")
-    WebElement delete;
+public final class PlaceholderPage extends EntityBaseTablePage<PlaceholderPage, PlaceholderEditPage, PlaceholderViewPage> {
 
     public PlaceholderPage(WebDriver driver) {
         super(driver);
     }
 
     @Override
-    protected PlaceholderEdit1Page createEditPage() {
-        return new PlaceholderEdit1Page(getDriver());
+    protected PlaceholderEditPage createEditPage() {
+        return new PlaceholderEditPage(getDriver());
     }
 
     @Override
@@ -51,30 +27,17 @@ public final class PlaceholderPage extends EntityBaseTablePage<PlaceholderPage, 
         return new PlaceholderViewPage(getDriver());
     }
 
-    public PlaceholderEditPage createNewRecord(){
-        newFolderButton.click();
-        return new PlaceholderEditPage(getDriver());
+    @Override
+    public List<String> getRow(int rowNumber) {
+        return getRows().get(rowNumber).findElements(By.tagName("td")).stream()
+                .map(WebElement::getText).collect(Collectors.toList()).subList(1, 10);
     }
 
-    public PlaceholderPage verify (String stringValue, String textValue, String integerValue, String decimalValue) {
-        Assert.assertEquals(el1.getText(), stringValue);
-        Assert.assertEquals(el2.getText(), textValue);
-        Assert.assertEquals(el3.getText(), integerValue);
-        Assert.assertEquals(el4.getText(), decimalValue);
-        return this;
+    public String getTitle(int rowNumber) {
+        return getRow(rowNumber).get(0);
     }
 
-    public List<WebElement> newRecordElements() {
-        List<WebElement> listOfElements = new ArrayList<>();
-        listOfElements.add(el1);
-        listOfElements.add(el2);
-        listOfElements.add(el3);
-        listOfElements.add(el4);
-        return listOfElements;
-    }
-
-    public void deleteRecord (){
-        ProjectUtils.click(getDriver(), actions);
-        ProjectUtils.click(getDriver(), delete);
+    public String getDecimal(int rowNumber) {
+        return getRow(rowNumber).get(3);
     }
 }
