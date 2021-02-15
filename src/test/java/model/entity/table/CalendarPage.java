cf18b6fa1cdcda4d7d9680a2b0fbfc1228ddf655
Svitlana <varakuta3112@gmail.com>
2021-02-15 11:13:49 -0600

    added POM for editCalendar and deleteCalendar. Changed xpath for List

--- src/test/java/model/entity/table/CalendarPage.java
+++ src/test/java/model/entity/table/CalendarPage.java
@@ -14,7 +14,7 @@ import java.util.stream.Collectors;
 
 public final class CalendarPage extends EntityBaseTablePage<CalendarPage, CalendarEditPage, CalendarViewPage> {
 
-    @FindBy(xpath = ("//ul[@role='tablist']//i[contains(text(),'list')]"))
+    @FindBy(xpath = ("//div[@class='content']//li[2]"))
     private WebElement clickList;
 
     @FindBy(xpath = "//tbody/tr")
