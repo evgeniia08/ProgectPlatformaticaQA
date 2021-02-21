c19907784d02d73504af42bace6dc188167fac7a
ke-de-ve <kedeve@gmail.com>
2021-02-20 20:11:57 -0500

    refactored existing Read-Only entity to POM

---
+++ src/test/java/model/entity/view/ReadOnlyViewPage.java
@@ -0,0 +1,11 @@
+package model.entity.view;
+
+import model.base.EntityBaseViewPage;
+import org.openqa.selenium.WebDriver;
+
+public class ReadOnlyViewPage extends EntityBaseViewPage {
+
+    public ReadOnlyViewPage(WebDriver driver) {
+        super(driver);
+    }
+}
